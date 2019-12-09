ec6f77b6895e30f357a4f2cb768c16d733db5df4
you9you <277893117@qq.com>
2019-12-09 10:16:59 +0800

    fix_191209
    调整更多的旧框架

--- js/index.js
+++ js/index.js
@@ -28,69 +28,128 @@
                 });
 
                 $('header ul.nav').html(function () {
-                        let ret = "";
-                        for (let i = 0; i < json['head'].length; i++) {
-                            let j = json['head'][i];
-                            switch (j['type']) {
-                                case "active":
-                                    ret += '<li class="active"><a href="' + j['link'] + '">' + j['title'] + '</a></li>';
-                                    break;
-                                case "normal":
-                                    ret += '<li><a href="' + j['link'] + '">' + j['title'] + '</a></li>';
-                                    break;
-                                case "return":
-                                    ret += '<li><a href="/">' + j['title'] + '</a></li>';
-                                    break;
-                            }
+                    let ret = "";
+                    for (let i = 0; i < json['head'].length; i++) {
+                        let j = json['head'][i];
+                        switch (j['type']) {
+                            case "active":
+                                ret += '<li class="active"><a href="' + j['link'] + '">' + j['title'] + '</a></li>';
+                                break;
+                            case "normal":
+                                ret += '<li><a href="' + j['link'] + '">' + j['title'] + '</a></li>';
+                                break;
+                            case "return":
+                                ret += '<li><a href="/">' + j['title'] + '</a></li>';
+                                break;
                         }
-                        return ret;
                     }
-                );
+                    return ret;
+                });
 
 
-                $('section#intro div.intro').html(function () {
-                        return json['intro'];
+                $('section#intro div.row div').html(function () {
+                    if (json['intro'] == null) {
+                        $('section#intro').css('display', 'none');
+                        return "";
                     }
-                );
+
+                    return '<div class="intro animate-box">' + json['intro'] + '</div>';
+                });
 
                 $('main#main div.container').html(function () {
-                        let ret = "";
-                        for (let i = 0; i < json['main'].length; i++) {
-                            let j = json['main'][i];
-                            ret += '<div class="offset-md-2 col-md-8 animate-box">' + j + '</div>';
-                        }
-                        return ret;
+                    if (json['main'] == null) {
+                        $('main#main').css('display', 'none');
+                        return "";
                     }
-                );
+                    let ret = "";
+                    for (let i = 0; i < json['main'].length; i++) {
+                        let j = json['main'][i];
+                        ret += '<div class="offset-md-2 col-md-8 animate-box">' + j + '</div>';
+                    }
+                    return ret;
+                });
+
+                $('section#work div.container').html(function () {
+                    if (json['work'] == null) {
+                        $('section#work').css('display', 'none');
+                        return "";
+                    }
+                    let ret = "";
+                    ret += '<div class="row">';
+                    for (let i = 0; i < json['work'].length; i++) {
+                        let j = json['work'][i];
+                        ret += '<div class="' + j['class'] + '">';
+                        ret += '<div class="fh5co-grid animate-box" style="background-image: url(' + j['image'] + ');">';
+                        ret += '<a class="image-popup text-center">';
+                        ret += '<div class="work-title">';
+                        ret += '<h3>' + j['title-h3'] + '</h3>';
+                        ret += '<span>' + j['title-span'] + '</span>';
+                        ret += '</div></a></div></div>';
+                    }
+                    ret += '</div>';
+                    return ret;
+                });
+
+                $('section#product div.container').html(function () {
+                    if (json['product'] == null) {
+                        $('section#product').css('display', 'none');
+                        return "";
+                    }
+                    let ret = "";
+                    ret += '<div class="row animate-box">';
+                    ret += '<div class="col-md-12 section-heading text-center">';
+                    ret += '<h2>' + json['product']['heading'] + '</h2>';
+                    ret += '<div class="row">';
+                    ret += '<div class="col-md-6 offset-md-3 subtext">';
+                    ret += '<p>' + json['product']['subtext'] + '</p>';
+                    ret += '</div></div></div></div>';
+                    ret += '<div class="row post-entry">';
+
+
+                    for (let i = 0; i < json['product']['block'].length; i++) {
+                        let j = json['product']['block'][i];
+                        ret += '<div class="col-md-6">';
+                        ret += '<div class="post animate-box">';
+                        ret += '<a><img src="' + j['image'] + '" alt="Product"></a>';
+                        ret += '<div><h3>' + j['title'] + '</h3>';
+                        ret += '<p>' + j['paragraph'] + '</p>';
+                        ret += '<span><a href="' + j['link'] + '">' + j['link_title'] + '</a></span>';
+                        ret += '</div></div></div>';
+                    }
+                    ret += '</div>';
+                    return ret;
+                });
+
 
                 $('section#services div.row').html(function () {
-                        let ret = "";
-                        for (let i = 0; i < json['services'].length; i++) {
-                            let j = json['services'][i];
-                            ret += '<div class="col-md-4 animate-box">';
-                            ret += '<div class="service">';
-                            ret += '<div class="service-icon">';
-                            ret += '<i class="' + j['icon'] + '"></i>';
-                            ret += '</div>';
-                            ret += '<h2>' + j['title'] + '</h2>';
-                            ret += '<p>' + j['content'] + '</p>';
-                            ret += '</div>';
-                            ret += '</div>';
-                        }
-                        return ret;
+                    if (json['services'] == null) {
+                        $('section#services').css('display', 'none');
+                        return "";
                     }
-                );
+                    let ret = "";
+                    for (let i = 0; i < json['services'].length; i++) {
+                        let j = json['services'][i];
+                        ret += '<div class="col-md-4 animate-box">';
+                        ret += '<div class="service">';
+                        ret += '<div class="service-icon">';
+                        ret += '<i class="' + j['icon'] + '"></i>';
+                        ret += '</div>';
+                        ret += '<h2>' + j['title'] + '</h2>';
+                        ret += '<p>' + j['content'] + '</p>';
+                        ret += '</div>';
+                        ret += '</div>';
+                    }
+                    return ret;
+                });
 
                 $('footer#footer p.pull-left small').html(function () {
-                        return json['footer']['left'];
-                    }
-                );
+                    return json['footer']['left'];
+                });
 
                 $('footer#footer p.pull-right small').html(function () {
-                        return json['footer']['right'];
-                    }
-                );
-                $('ul.nav a').on('click',c.navOnClick);
+                    return json['footer']['right'];
+                });
+                $('ul.nav a').on('click', c.navOnClick);
 
                 $(function () {
                     let i = 0;
@@ -125,7 +184,7 @@
 
     c.route = function (route) {
         c.loadData(
-            '/data' + route[0] + '_' + c.getLang() + '.json'
+            '/data' + route[0] + route[1] + '_' + c.getLang() + '.json'
         );
     };
     c.Modal_show = function (title, body) {
@@ -137,7 +196,7 @@
         $('div#Modal').modal('show');
     };
 
-    c.navOnClick = function(){
+    c.navOnClick = function () {
         $('ul.nav li').removeClass('active');
         $(this).parent().addClass('active');
 
